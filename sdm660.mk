1db400db2cffcf5a5601f0c95866eb0ee8b97c87
Danny Baumann <dannybaumann@web.de>
2022-07-27 21:43:01 +0530

    sdm660-common: Add touchscreen gestures HIDL HAL
    Change-Id: I02380e387f01fe20f984d2e6daa095f2056c03f9
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- sdm660.mk
+++ sdm660.mk
@@ -443,6 +443,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.sdm660
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.X00TD
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service

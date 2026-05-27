c58c1471dfbd6d03d2ce66bcc6a5dc99f2cc7e41
texascake <tiktodz@soulvibe-team.cloud>
2026-05-27 07:29:17 +0000

    sdm660-common: rootdir: migrate tunables from post-boot to init.qcom.power
    Change-Id: I0e933da4b41d800384dffca81ccd0ab7c0c36905

--- sdm660.mk
+++ sdm660.mk
@@ -411,6 +411,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.msm.usb.configfs.rc \
     init.qcom.asus.rc \
+    init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
